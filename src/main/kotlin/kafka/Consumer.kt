75aef755029546ce50de836b37591164f15d9029
rudykang-nsuslab <rudy.kang@nsuslab.com>
2021-11-19 18:56:55 +0900

    kafka message를 db 저장까지 연결

--- src/main/kotlin/kafka/Consumer.kt
+++ src/main/kotlin/kafka/Consumer.kt
@@ -2,14 +2,16 @@ package kafka
 
 import dto.Person
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import repository.sqlite.UserRepositoryImpl
 import utils.ConfigManager
 import java.time.Duration
 
 class Consumer {
     //    private val topic = "quickstart-events"
-    private val topic = "object-test-topic"
+    private val topic = "object-save-test-topic"
 
     fun run() {
+        val repo = UserRepositoryImpl()
 
         println("Set properties...")
         val prop = ConfigManager.loadKafkaConfig()
@@ -20,10 +22,20 @@ class Consumer {
         consumer.subscribe(listOf(topic))
 
         println("Getting messages")
-        for (i in 1..3) {
+        while (true) {
             val records = consumer.poll(Duration.ofMillis(1000))
             println(records.count())
-            records.forEach { println(it.value()) }
+            records.forEach {
+                val p = it.value()
+                println(p)
+                if (p != null) {
+                    if (repo.queryPerson(p.id) != null) {
+                        repo.updatePerson(p)
+                    } else {
+                        repo.createPerson(p)
+                    }
+                }
+            }
         }
     }
 }
